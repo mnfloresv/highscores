b634d6542dfcdd59bfc9931af4ed5870619f0e04
Manuel Flores <manuelfloresv@gmail.com>
2017-10-23 18:11:06 +0200

    Add validators to score fields

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,6 @@
 class Score < ApplicationRecord
   belongs_to :game
+  validates :player_email, :score, presence: true
+  validates :score, numericality: { greater_than_or_equal_to: 0 }
+  validates :player_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
