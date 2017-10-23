8a48f0045e5ad9f43793461b61e0e0103735635c
Manuel Flores <manuelfloresv@gmail.com>
2017-10-23 22:50:35 +0200

    Process scores POST request from form and API

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,5 +4,23 @@ class ScoresController < ApplicationController
   end
 
   def create
+    @score = Score.new(score_params)
+
+    respond_to do |format|
+      if @score.save
+        format.html { redirect_to scores_new_path, notice: 'Score was successfully added.' }
+        format.json { render json: @score, status: :created}
+
+      else
+        format.html { render :new }
+        format.json { render json: @score.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+
+    def score_params
+      params.require(:score).permit(:player_email, :game_id, :score)
+    end
 end
